c506c1ce92b240b7e6cb2fc1adc75098f9a767a5
digitaltsai <digitaltsai@gmail.com>
2014-06-26 11:14:42 -0700

    simple serverside

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 var express = require('express');
 var app = express();
 
+var TokShow = {
+  state: "pending",
+  startTime: null
+};
+
+app.use(express.bodyParser());
+
+app.get("/state", function(req, res) {
+  res.send(200, TokShow);
+});
+
+app.post("/state", function(req, res) {
+  if (req.body.state) {
+    TokShow.state = req.body.state;
+  }
+
+  if (req.body.startTime) {
+    TokShow.startTime = req.body.startTime;
+  }
+
+  res.send(200, TokShow);
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
